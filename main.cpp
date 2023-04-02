d35f68e6adbada7a9876b52fc144e067c046af98
IvanFedoruk2302 <125930483+IvanFedoruk2302@users.noreply.github.com>
2023-04-02 23:49:04 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class type1 {
+public:
+    void Get_answer() {
+        cout << "Ответ: Любое значение Х" << endl;
+    }
+    void show() {
+        cout << "Уравнение:  0 = 0" << endl;
+    }
+};
+
+class type2 {
+private:
+    double A;
+public:
+    type2(double a1) {
+        A = a1;
+    }
+    void Get_answer() {
+        cout << "Ответ:  Х = 0" << endl;
+    }
+    void show() {
+        cout << "Уравнение " << A << "* x^2 = 0" << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Первый пример: " << endl;
+    type1 Example1;
+    Example1.Get_answer();
+    Example1.show();
+    cout << "Второй пример: " << endl;
+    type2 Example2(4);
+    Example2.Get_answer();
+    Example2.show();
+    return 0;
+}
