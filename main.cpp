1eaa13c00333f4cb92d77d88cd4ffc3505991b7c
IvanFedoruk2302 <125930483+IvanFedoruk2302@users.noreply.github.com>
2023-04-16 23:37:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,6 +27,45 @@ public:
     }
 };
 
+class type3 {
+private:
+    double C;
+public:
+    type3(double c1) {
+        C = c1;
+    }
+    void Get_answer() {
+        cout << "Корней нет" << endl;
+    }
+    void show() {
+        cout << C << " = 0" << endl;
+    }
+};
+
+class type4 {
+private:
+    double A, C;
+public:
+    type4(double a1, double c1) {
+        A = a1;
+        C = c1;
+    }
+    void Get_answer() {
+        double k = ((-1) * C) / A;
+        if (k >= 0) {
+            double x1 = sqrt(k);
+            double x2 = sqrt(k) * (-1);
+            cout << "Корни уравнения: " << endl << "X1 = " << x1 << endl << "X2 = " << x2 << endl;
+        }
+        else {
+            cout << "Корней нет" << endl;
+        }
+    }
+    void show() {
+        cout << "Уравнение " << A << "*x^2 + " << C << " = 0" << endl;
+    }
+};
+
 int main()
 {
     SetConsoleCP(1251);
@@ -36,9 +75,22 @@ int main()
     type1 Example1;
     Example1.Get_answer();
     Example1.show();
+
     cout << "Второй пример: " << endl;
-    type2 Example2(4);
+    type2 Example2(3);
     Example2.Get_answer();
     Example2.show();
+
+    cout << "Третий пример: " << endl;
+    type3 Example3(3);
+    Example3.Get_answer();
+    Example3.show();
+
+    cout << "Четверый пример: " << endl;
+    type4 Example4(1, -25);
+    Example4.Get_answer();
+    Example4.show();
+
     return 0;
+
 }
