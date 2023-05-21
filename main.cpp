84ba5d2d7528b4439e191955148a3846a1b493a3
IvanFedoruk2302 <125930483+IvanFedoruk2302@users.noreply.github.com>
2023-05-21 22:08:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <Windows.h>
+#include <cmath>
 using namespace std;
 
 class type1 {
 public:
     void Get_answer() {
-        cout << "Ответ: Любое значение Х" << endl;
+        cout << "Ответом является любое значение Х" << endl;
     }
     void show() {
-        cout << "Уравнение:  0 = 0" << endl;
+        cout << "Уравнение 0 = 0" << endl;
     }
 };
 
@@ -20,10 +20,10 @@ public:
         A = a1;
     }
     void Get_answer() {
-        cout << "Ответ:  Х = 0" << endl;
+        cout << "Ответом является Х = 0" << endl;
     }
     void show() {
-        cout << "Уравнение " << A << " * x^2 = 0" << endl;
+        cout << "Уравнение " << A << "*x^2 = 0" << endl;
     }
 };
 
@@ -64,6 +64,23 @@ public:
     void show() {
         cout << "Уравнение " << A << "*x^2 + " << C << " = 0" << endl;
     }
+    void Get_answer_podbor() {
+        if ((((-1) * C) / A) < 0) {
+            cout << "Корней методом подбора нет" << endl;
+        }
+        else {
+            double min = abs(0 - A * (-20) * (-20) - C);
+            double x = -20;
+            for (double i = -20; i < 20.01; i = i + 0.01) {
+                double temp = abs(0 - A * (i) * (i)-C);
+                if (temp < min) {
+                    min = temp;
+                    x = i;
+                }
+            }
+            cout << "Приближенно, один из корней уравнения: " << x << endl;
+        }
+    }
 };
 
 class type5 {
@@ -80,6 +97,18 @@ public:
     void show() {
         cout << "Уравнение " << B << "*x + " << C << " = 0" << endl;
     }
+    void Get_answer_podbor() {
+        double min = abs(0 - B * (-20) - C);
+        double x = -20;
+        for (double i = -20; i < 20.01; i = i + 0.01) {
+            double temp = abs(0 - B * (i)-C);
+            if (temp < min) {
+                min = temp;
+                x = i;
+            }
+        }
+        cout << "Приближенно, один из корней уравнения: " << x << endl;
+    }
 };
 
 class type6 {
@@ -94,7 +123,7 @@ public:
     void Get_answer() {
         double D = B * B - 4 * A * C;
         if (D < 0) {
-            cout << "Уравнение не имеет действительных корней " << endl;
+            cout << "Уравнение не имеет действительных корней" << endl;
         }
         else {
             if (D == 0) {
@@ -112,85 +141,136 @@ public:
     void show() {
         cout << "Уравнение " << A << "*x^2 + " << B << "*x + " << C << " = 0" << endl;
     }
+    void Get_answer_podbor() {
+        double D = B * B - 4 * A * C;
+        if (D < 0) {
+            cout << "Уравнение не имеет действительных корней" << endl;
+        }
+        else {
+            double min = abs(0 - A * (-20) * (-20) - B * (-20) - C);
+            double x = -20;
+            for (double i = -20; i < 20.01; i = i + 0.01) {
+                double temp = abs(0 - A * (i) * (i)-B * i - C);
+                if (temp < min) {
+                    min = temp;
+                    x = i;
+                }
+            }
+            cout << "Приближенно, один из корней уравнения: " << x << endl;
+        }
+    }
 };
-    
+
 class type7 {
-    private:
+private:
     double B;
-    public:
-    type7 (double b1) {
+public:
+    type7(double b1) {
         B = b1;
     }
-    void Get_answer () {
+    void Get_answer() {
         cout << "Корень уравнения: x = 0" << endl;
     }
-    void show () {
+    void show() {
         cout << "Уравнение " << B << "*x = 0" << endl;
     }
 };
 
 class type8 {
-    private:
+private:
     double A, B;
-    public:
-    type8 (double a1, double b1) {
+public:
+    type8(double a1, double b1) {
         A = a1;
         B = b1;
     }
-    void Get_answer () {
-                double x = ((-1)*B)/A;
-                cout << "Корни уравнения: " << endl;
-                cout << "X1 = 0" << endl << "X2 = " << x << endl;
+    void Get_answer() {
+        double x = ((-1) * B) / A;
+        cout << "Корни уравнения: " << endl;
+        cout << "X1 = 0" << endl << "X2 = " << x << endl;
     }
-    void show () {
+    void show() {
         cout << "Уравнение " << A << "*x^2 + " << B << "*x = 0 " << endl;
+    }
+    void Get_answer_podbor() {
+        double min = abs(0 - A * (-20) * (-20) - B * (-20));
+        double x = -20;
+        for (double i = -20; i < 20.01; i = i + 0.01) {
+            if (i != 0) {
+                double temp = abs(0 - A * i * i - B * (i));
+                if (temp < min) {
+                    min = temp;
+                    x = i;
+                }
+            }
+        }
+        cout << "Приближенно, один из корней уравнения: " << x << endl;
     }
 };
 
 int main()
-{
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    setlocale(LC_ALL, "ru");
-    cout << "Первый пример: " << endl;
-    type1 Example1;
-    Example1.Get_answer();
-    Example1.show();
+{   
+    system("color 3F");
+    setlocale(LC_ALL, "Russian");
+    double A, B, C;
+    cout << "Введите коэффициенты 1-го уравнения A, B и C через пробел: " << endl;
+    cin >> A >> B >> C;
+    type1 eq1;
 
-    cout << "Второй пример: " << endl;
-    type2 Example2(3);
-    Example2.Get_answer();
-    Example2.show();
+    cout << "Введите коэффициенты 2-го уравнения A, B и C через пробел: " << endl;
+    cin >> A >> B >> C;
+    type2 eq2(B);
 
-    cout << "Третий пример: " << endl;
-    type3 Example3(3);
-    Example3.Get_answer();
-    Example3.show();
+    cout << "Введите коэффициенты 3-го уравнения A, B и C через пробел: " << endl;
+    cin >> A >> B >> C;
+    type3 eq3(C);
 
-    cout << "Четверый пример: " << endl;
-    type4 Example4(1, -25);
-    Example4.Get_answer();
-    Example4.show();
+    cout << "Введите коэффициенты 4-го уравнения A, B и C через пробел: " << endl;
+    cin >> A >> B >> C;
+    type4 eq4(A, C);
 
-    cout << "Пятый пример: " << endl;
-    type5 Example5(10,20);
-    Example5.Get_answer();
-    Example5.show();
+    cout << "Введите коэффициенты 5-го уравнения A, B и C через пробел: " << endl;
+    cin >> A >> B >> C;
+    type5 eq5(B, C);
 
-    cout << "Шестой пример: " << endl;
-    type6 Example6(1,5,6);
-    Example6.Get_answer();
-    Example6.show();
-    
-    cout << "Седьмой пример: " << endl;
-    type7 Example7(5);
-    Example7.Get_answer();
-    Example7.show();
-    
-    cout << "Восьмой пример: " << endl;
-    type8 Example8(1,5);
-    Example8.Get_answer();
-    Example8.show();
+    cout << "Введите коэффициенты 6-го уравнения A, B и C через пробел: " << endl;
+    cin >> A >> B >> C;
+    type6 eq6(A, B, C);
+
+    cout << "Введите коэффициенты 7-го уравнения A, B и C через пробел: " << endl;
+    cin >> A >> B >> C;
+    type7 eq7(B);
+
+    cout << "Введите коэффициенты 8-го уравнения A, B и C через пробел: " << endl;
+    cin >> A >> B >> C;
+    type8 eq8(A, B);
+
+    eq1.show();
+    eq1.Get_answer();
+
+    eq2.show();
+    eq2.Get_answer();
+
+    eq3.show();
+    eq3.Get_answer();
+
+    eq4.show();
+    eq4.Get_answer();
+    eq4.Get_answer_podbor();
+
+    eq5.show();
+    eq5.Get_answer();
+    eq5.Get_answer_podbor();
+
+    eq6.show();
+    eq6.Get_answer();
+    eq6.Get_answer_podbor();
+
+    eq7.show();
+    eq7.Get_answer();
+
+    eq8.show();
+    eq8.Get_answer();
+    eq8.Get_answer_podbor();
     return 0;
-
 }
